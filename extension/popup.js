dffee92e9aea1e980e5765ad7230fef8ca1fca2a
santonov62 <flex62ryz@ya.ru>
2019-04-11 18:04:55 +0300

    Ui

--- extension/popup.js
+++ extension/popup.js
@@ -1,28 +1,43 @@
-let changeColor = document.getElementById('changeColor');
+window.onload = async () => {
+    showSpinner();
+    const orders = await loadOrders();
+    renderOrders(orders);
+    hideSpinner();
+};
 
-chrome.storage.sync.get('color', function(data) {
-  changeColor.style.backgroundColor = data.color;
-  changeColor.setAttribute('value', data.color);
-});
+const renderOrders = (orders = []) => {
+    const ul = document.querySelector('#orders ul');
+    orders.forEach((order) => {
+        const li = document.createElement('li');
+        ul.appendChild(li);
+        li.innerHTML = `${order.name}`;
+    });
+};
 
-window.onload = () => {
-  fetch('https://delivery-group-order.herokuapp.com/')
-      .then(
-          function(response) {
-            if (response.status !== 200) {
-              console.log('Looks like there was a problem. Status Code: ' +
-                  response.status);
-              return;
+const loadOrders = () => {
+    return fetch('https://delivery-group-order.herokuapp.com/')
+        .then(
+            function (response) {
+                if (response.status !== 200) {
+                    console.log('Looks like there was a problem. Status Code: ' +
+                        response.status);
+                    return;
+                }
+
+                return response.json();
             }
+        )
+        .catch(function (err) {
+            console.log('Fetch Error :-S', err);
+        });
+};
 
-            // Examine the text in the response
-            response.json().then(function(data) {
-              console.log(data);
-            });
+const showSpinner = () => {
+    const spinner = document.querySelector(`.loader`);
+    spinner.classList.add(`show`);
+};
 
-          }
-      )
-      .catch(function(err) {
-        console.log('Fetch Error :-S', err);
-      });
+const hideSpinner = () => {
+    const spinner = document.querySelector(`.loader`);
+    spinner.classList.add(`hide`);
 };
