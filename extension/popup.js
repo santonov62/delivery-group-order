db0ed72404bd0e7d71f33df2a05667f5077053e5
santonov62 <flex62ryz@ya.ru>
2019-04-11 16:04:52 +0300

    fetch data

--- extension/popup.js
+++ extension/popup.js
@@ -4,3 +4,25 @@ chrome.storage.sync.get('color', function(data) {
   changeColor.style.backgroundColor = data.color;
   changeColor.setAttribute('value', data.color);
 });
+
+window.onload = () => {
+  fetch('https://delivery-group-order.herokuapp.com/')
+      .then(
+          function(response) {
+            if (response.status !== 200) {
+              console.log('Looks like there was a problem. Status Code: ' +
+                  response.status);
+              return;
+            }
+
+            // Examine the text in the response
+            response.json().then(function(data) {
+              console.log(data);
+            });
+
+          }
+      )
+      .catch(function(err) {
+        console.log('Fetch Error :-S', err);
+      });
+};
