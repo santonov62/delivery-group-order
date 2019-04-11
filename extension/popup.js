656ce68fb02999417a2f5e78c7a41b689109cc4d
santonov62 <flex62ryz@ya.ru>
2019-04-11 18:29:29 +0300

    Spinner

--- extension/popup.js
+++ extension/popup.js
@@ -10,7 +10,14 @@ const renderOrders = (orders = []) => {
     orders.forEach((order) => {
         const li = document.createElement('li');
         ul.appendChild(li);
-        li.innerHTML = `${order.name}`;
+        const a = document.createElement('a');
+        const title = `${order.shop && order.shop.name} (${order.address})`;
+        const linkText = document.createTextNode(title);
+        a.appendChild(linkText);
+        a.title = title;
+        a.href = order.url;
+        a.target = 'blank';
+        li.appendChild(a);
     });
 };
 
@@ -19,8 +26,7 @@ const loadOrders = () => {
         .then(
             function (response) {
                 if (response.status !== 200) {
-                    console.log('Looks like there was a problem. Status Code: ' +
-                        response.status);
+                    console.log('Looks like there was a problem. Status Code: ' + response.status);
                     return;
                 }
 
